726534b37482196e50796984bad3de1efa84819d
Mariya <mariya.shestakova.777@gmail.com>
2024-03-10 19:27:59 +0300

    Updated commit

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -2,36 +2,9 @@ from django.contrib import admin
 from django.core.exceptions import ValidationError
 from django.forms import BaseInlineFormSet
 
-from .models import Article, Scope, ArticleScope
+from .models import Article
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ['title', 'text', 'published_at', 'image']
-    list_filter = ['published_at']
-
-# @admin.register(ArticleScope)
-# class ArticleScopeAdmin(admin.ModelAdmin):
-#     list_display = ['tag', 'article', 'scopes']
-#     list_filter = ['tag']
-
-class ArticleScopeInlineFormset(BaseInlineFormSet):
-    def clean(self):
-        for form in self.forms:
-            # В form.cleaned_data будет словарь с данными
-            # каждой отдельной формы, которые вы можете проверить
-            form.cleaned_data
-            # вызовом исключения ValidationError можно указать админке о наличие ошибки
-            # таким образом объект не будет сохранен,
-            # а пользователю выведется соответствующее сообщение об ошибке
-            raise ValidationError('Тут всегда ошибка')
-        return super().clean()  # вызываем базовый код переопределяемого метода
-
-
-class ArticleScopeInline(admin.TabularInline):
-    model = ArticleScope
-    formset = ArticleScopeInlineFormset
-
-@admin.register(Scope)
-class ScopeAdmin(admin.ModelAdmin):
-    inlines = [ArticleScopeInline,]
+    pass
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 
-
+class Tag(models.Model):
+    name = models.CharField(max_length=256, verbose_name='Тематика статьи')
 class Article(models.Model):
 
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    scope = models.ManyToManyField(Tag, through='Scope')
 
     class Meta:
         verbose_name = 'Статья'
@@ -16,9 +18,8 @@ class Article(models.Model):
     def __str__(self):
         return self.title
 class Scope(models.Model):
-    articles = models.ManyToManyField(Article, related_name='orders', through='ArticleScope')
+    articles = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    is_main = models.BooleanField(default=False)
 
-class ArticleScope(models.Model):
-    tag = models.CharField(max_length=256, verbose_name='Название')
-    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='positions')
-    scopes = models.ForeignKey(Scope, on_delete=models.CASCADE, related_name='positions')
+
