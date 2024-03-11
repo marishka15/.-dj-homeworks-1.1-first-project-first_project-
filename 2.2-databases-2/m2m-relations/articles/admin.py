c412656557d1a95c337083fe0c4e27551addf629
Mariya <mariya.shestakova.777@gmail.com>
2024-03-11 13:06:42 +0300

    Updated commit

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -2,9 +2,32 @@ from django.contrib import admin
 from django.core.exceptions import ValidationError
 from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Scope, Tag
 
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        i = 0
+        for form in self.forms:
+            dictionary = form.cleaned_data
+            if dictionary.get('is_main'):
+                i += 1
+            else:
+                continue
+        if i == 0:
+            raise ValidationError('Выберите главную тему')
+        elif i > 1:
+            raise ValidationError('Тут всегда ошибка')
+        return super().clean()
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ScopeInline]
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
     pass
--- 2.2-databases-2/m2m-relations/articles/views.py
+++ 2.2-databases-2/m2m-relations/articles/views.py
@@ -5,8 +5,6 @@ from articles.models import Article
 
 def articles_list(request):
     template = 'articles/news.html'
-
-
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/3.1/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     ordering = '-published_at'
